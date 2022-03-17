fb577691829984b9da2f163babb802ed32e20d4d
Lino Jimenez <ljimenez@LJIMEMAC2.local>
2022-03-16 21:55:08 -0500

    initial commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,10 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get("/api/whoami", function (req, res) {
+  console.log("1", req);
+  res.json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] });
 });
 
 
